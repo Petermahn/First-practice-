23802e59908f32cf1a0d552686bb412f237dbe4e
Petermahn <110340716+Petermahn@users.noreply.github.com>
2022-09-20 00:37:42 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+prompt("Peters registration form")
+
+let button = document.querySelector("button").addEventListener("dblclick", red)
+function red(){
+    alert("you have successfully submited")
+}
+
+document.querySelector("#door").addEventListener("mouseover", green)
+function green(){
+    alert("Please make sure you've correctly fill out the form")
+}
+
+document.addEventListener("mousemove", A)
+function A(){
+    const X="numbers of my mouse movement"
+
+    console.log(X)
+}
